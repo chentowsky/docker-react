d7c268c657d920c7f77d0b3ea5c8ae437882314f
@chentowsky <vicente.ruiz.vasquez@gmail.com>
2020-05-09 13:12:01 +0200

    Change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola mundo!
+          Hola mundo modificado en la rama feature!
         </p>
         <a
           className="App-link"
